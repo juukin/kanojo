3f7eb64e77eb56719a0557e40cda559e116ec76c
juukin <32163888+juukin@users.noreply.github.com>
2017-12-08 14:25:30 +0400

    Update createtable.php

--- createtable.php
+++ createtable.php
@@ -1,7 +1,7 @@
 <?php
 echo "123456";
 try {
-    $conn = new PDO("sqlsrv:server = tcp:sqlp.database.windows.net,1433; Database = sql","sqlp","sqlplz");
+    $conn = new PDO("sqlsrv:server = tcp:sqlp.database.windows.net,1433; Database = sql","sqlp","200487pp");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
    
     $sql = "CREATE TABLE registration_tbl(
