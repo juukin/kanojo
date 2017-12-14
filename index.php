229e06d68ebe4aaf64695afaa957411af6ec5ee1
juukin <32163888+juukin@users.noreply.github.com>
2017-12-14 13:12:26 +0200

    Update index.php

--- index.php
+++ index.php
@@ -57,7 +57,7 @@ $vopros = $_POST['vopros'];
 $otvet = $_POST['otvet']; 
 
 $sql_insert = 
-"INSERT INTO registration_tbl1 (name, email, date) 
+"INSERT INTO registration_tbl (name, email, date) 
 VALUES (?,?,?)"; 
 $stmt = $conn->prepare($sql_insert); 
 $stmt->bindValue(1, $name); 
@@ -70,7 +70,7 @@ die(var_dump($e));
 } 
 echo "<h3>Your're registered!</h3>"; 
 } 
-$sql_select = "SELECT * FROM registration_tbl1"; 
+$sql_select = "SELECT * FROM registration_tbl"; 
 $stmt = $conn->query($sql_select); 
 $registrants = $stmt->fetchAll(); 
 if(count($registrants) > 0) { 
