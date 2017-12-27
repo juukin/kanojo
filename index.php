d536def765c404790cf953eccfeeb5d16ef7199c
juukin <32163888+juukin@users.noreply.github.com>
2017-12-28 03:10:15 +0400

    Update index.php

--- index.php
+++ index.php
@@ -1,95 +1,121 @@
-<html> 
-<head> 
-<Title>Registration Form</Title> 
-<style type="text/css"> 
-body { background-color: 
-#fff; border-top: solid 10px #000; 
-color: #333; font-size: .85em; 
-margin: 20; padding: 20; 
-font-family: "Segoe UI", 
-Verdana, Helvetica, Sans-Serif; 
-} 
-h1, h2, h3,{ color: #000; 
-margin-bottom: 0; padding-bottom: 0; } 
-h1 { font-size: 2em; } 
-h2 { font-size: 1.75em; } 
-h3 { font-size: 1.2em; } 
-table { margin-top: 0.75em; } 
-th { font-size: 1.2em; 
-text-align: left; border: none; padding-left: 0; } 
-td { padding: 0.25em 2em 0.25em 0em; 
-border: 0 none; } 
-</style> 
-</head> 
-<body> 
-<h1>Регистрация</h1> 
-<p>Введите свое имя, адрес электронной почты, пароль, контрольный вопрос, ответ на него и нажмите кнопку <strong> Зарегестрироваться </strong>.</p> 
-<form method="post" action="index.php" 
-enctype="multipart/form-data" > 
-Имя <input type="text" 
-name="name" id="name"/></br> 
-Email <input type="text" 
-name="email" id="email"/></br> 
-Пароль <input type="text" 
-name="password" id="password"/></br> 
-Вопрос <input type="text" 
-name="vopros" id="vopros"/></br> 
-Ответ <input type="text" 
-name="otvet" id="otvet"/></br> 
-<input type="submit" 
-name="submit" value="Зарегестироваться" /> 
-<?php 
+<html>
+<head>
+<Title>Registration Form</Title>
+<style type="text/css">
+body { background-color:
+#fff; border-top: solid 10px #000;
+color: #333; font-size: .85em;
+margin: 20; padding: 20;
+font-family: "Segoe UI",
+Verdana, Helvetica, Sans-Serif;
+}
+h1, h2, h3,{ color: #000;
+margin-bottom: 0; padding-bottom: 0; }
+h1 { font-size: 2em; }
+h2 { font-size: 1.75em; }
+h3 { font-size: 1.2em; }
+table { margin-top: 0.75em; }
+th { font-size: 1.2em;
+text-align: left; border: none; padding-left: 0; }
+td { padding: 0.25em 2em 0.25em 0em;
+border: 0 none; }
+</style>
+</head>
+<body>
+<h1>Register here!</h1>
+<p>Fill in your name and
+email address, then click <strong>Submit</strong>
+to register.</p>
+<form method="post" action="index.php" enctype="multipart/form-data" >
+<input type ="text" name ="name" id ="name" placeholder ="Введите ваше имя">
+<input type ="text" name ="email" id ="email" placeholder ="Ваш еmail..">
+// Записываем данные в поля в email, name.
+<select name="country">
+<option value="">All</option>
+<option value="Russia">Russia</option>
+<option value="USA">USA</option>
+<option value="Germany">Germany</option>
+<option value="Japan">Japan</option>
+<option value="China">China</option>
+// Выбираем страну проживания
+</select>
+<input type ="submit" name ="submit" value ="Отправить">
+<input type="submit" name="filter" value="Фильтр">
+// Кнопки для регистрации и для фильтрации
+</form>
+
 try { 
 $conn = new PDO("sqlsrv:server = tcp:juuksqlserver.database.windows.net,1433; Database = juuksqlbase", "juuksqlserver", "200487pP"); 
-$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
-} 
-catch (PDOException $e) { 
-print("Error connecting to SQL Server."); 
-die(print_r($e)); 
-} 
-if(!empty($_POST)) { 
-try { 
-$name = $_POST['name']; 
-$email = $_POST['email']; 
-$date = date("Y-m-d"); 
-$password = $_POST['password']; 
-$vopros = $_POST['vopros']; 
-$otvet = $_POST['otvet']; 
-$sql_insert = 
-"INSERT INTO registration_tbl (name, email, date) 
-VALUES (?,?,?)"; 
-$stmt = $conn->prepare($sql_insert); 
-$stmt->bindValue(1, $name); 
-$stmt->bindValue(2, $email); 
-$stmt->bindValue(3, $password);
-$stmt->bindValue(4, $vopros);
-$stmt->bindValue(1, $otvet);
-$stmt->execute(); 
-} 
-catch(Exception $e) { 
-die(var_dump($e)); 
-} 
-echo "<h3>Your're registered!</h3>"; 
-} 
-$sql_select = "SELECT * FROM registration_tbl"; 
-$stmt = $conn->query($sql_select); 
-$registrants = $stmt->fetchAll(); 
-if(count($registrants) > 0) { 
-echo "<h2>People who are registered:</h2>"; 
-echo "<table>"; 
-echo "<tr><th>Name</th>"; 
-echo "<th>Email</th>"; 
-echo "<th>Password</th>"; 
-echo "<th>vopros</th>"; 
-echo "<th>otvet</th>"; 
-echo "<th>Date</th></tr>"; 
-foreach($registrants as $registrant) { 
-echo "<tr><td>".$registrant['name']."</td>"; 
-echo "<td>".$registrant['email']."</td>"; 
-echo "<td>".$registrant['date']."</td></tr>"; 
-} 
-echo "</table>"; 
-} else { 
-echo "<h3>No one is currently registered.</h3>"; 
-} 
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+try {
+$conn = new PDO($dsn, $username, $password);
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+// Для подключения к свое БД, используем строки подключения azure
+catch (PDOException $e) {
+print("Ошибка подключения к SQL Server.");
+die(print_r($e));
+}
+// Если не удалось подключиться то выводит это
+if(!empty($_POST)) {
+try {
+$name = $_POST['name'];
+$email = $_POST['email'];
+$date = date("Y-m-d");
+$country = $_POST['country'];
+// Присваивает значения
+if ($name == "" || $email == "") {
+echo "<h3>Не заполнены поля name и email.</h3>";
+// Сравнивает name и email
+}
+else {
+$sql_insert ="INSERT INTO registration_on (name, email, date, country) VALUES (?,?,?,?)";
+$stmt = $conn->prepare($sql_insert);
+$stmt->bindValue(1, $name);
+$stmt->bindValue(2, $email);
+$stmt->bindValue(3, $date);
+$stmt->bindValue(4, $country);
+$stmt->execute();
+// Добавляет в список регстрированных
+
+echo "<h3>Вы зарегистрировались!</h3>";
+}
+}
+catch(Exception $e) {
+die(var_dump($e));
+}
+}
+
+$sql_select = "SELECT * FROM registration_on";
+$stmt = $conn->query($sql_select);
+$stmt->execute();
+if(isset($_POST['filter'])) {
+$gender = $_POST['country'];
+$sql_select = "SELECT * FROM registration_on WHERE country like :country";
+$stmt = $conn->prepare($sql_select);
+$stmt->execute(array(':country'=>$country.'%'));
+// Производить фильтрацию по странам
+}
+$registrants = $stmt->fetchAll();
+if(count($registrants) > 0) {
+echo "<h2>Люди, которые зарегистрированы:</h2>";
+echo "<table>";
+echo "<tr><th>Name</th>";
+echo "<th>Email</th>";
+echo "<th>Country</th>";
+echo "<th>Date</th></tr>";
+foreach($registrants as $registrant) {
+echo "<td>".$registrant['name']."</td>";
+echo "<td>".$registrant['email']."</td>";
+echo "<td>".$registrant['country']."</td>";
+echo "<td>".$registrant['date']."</td></tr>";
+}
+echo "</table>";
+}
+else {
+echo "<h3>В настоящее время никто не зарегистрирован.</h3>";
+// Выводить данные о регистрированных 
+}
+
 ?>
