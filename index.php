01492fc8fe7ee091f205fa0297573ad5c6e259ba
juukin <32163888+juukin@users.noreply.github.com>
2017-12-28 03:31:41 +0400

    Update index.php

--- index.php
+++ index.php
@@ -49,7 +49,6 @@ try {
 $conn = new PDO($dsn, $username, $password);
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
-}
 catch (PDOException $e) {
 print("Ошибка подключения к SQL Server.");
 die(print_r($e));
