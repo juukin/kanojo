463d0de0c846ee317b807652c91e60083a592f23
juukin <32163888+juukin@users.noreply.github.com>
2017-12-28 13:14:40 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,110 +1,93 @@
-<html>
-<head>
-<Title>Registration Form</Title>
-<style type="text/css">
-body { background-color:
-#fff; border-top: solid 10px #000;
-color: #333; font-size: .85em;
-margin: 20; padding: 20;
-font-family: "Segoe UI",
-Verdana, Helvetica, Sans-Serif;
-}
-h1, h2, h3,{ color: #000;
-margin-bottom: 0; padding-bottom: 0; }
-h1 { font-size: 2em; }
-h2 { font-size: 1.75em; }
-h3 { font-size: 1.2em; }
-table { margin-top: 0.75em; }
-th { font-size: 1.2em;
-text-align: left; border: none; padding-left: 0; }
-td { padding: 0.25em 2em 0.25em 0em;
-border: 0 none; }
-</style>
-</head>
-<body>
-<h1>Register here!</h1>
-<p>Fill in your name and
-email address, then click <strong>Submit</strong>
-to register.</p>
-<form method="post" action="index.php" enctype="multipart/form-data" >
-<input type ="text" name ="name" id ="name" placeholder ="Введите ваше имя">
-<input type ="text" name ="email" id ="email" placeholder ="Ваш еmail..">
-<select name="country">
-<option value="">All</option>
-<option value="Russia">Russia</option>
-<option value="USA">USA</option>
-<option value="Germany">Germany</option>
-<option value="Japan">Japan</option>
-<option value="China">China</option>
-</select>
-<input type ="submit" name ="submit" value ="Отправить">
-<input type="submit" name="filter" value="Фильтр">
-</form>
-<?php
-$dsn = "sqlsrv:server = tcp:juuksqlserver.database.windows.net,1433; Database = juuksqlbase";
-$username = "juuksqlserver";
-$password = "200487pP";
-
-try {
-$conn = new PDO($dsn, $username, $password);
-$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $e) {
-print("Ошибка подключения к SQL Server.");
-die(print_r($e));
-}
-if(!empty($_POST)) {
-try {
-$name = $_POST['name'];
-$email = $_POST['email'];
-$date = date("Y-m-d");
-$country = $_POST['country'];
-if ($name == "" || $email == "") {
-echo "<h3>Не заполнены поля name и email.</h3>";
-}
-else {
-$sql_insert ="INSERT INTO registration_on (name, email, date, country) VALUES (?,?,?,?)";
-$stmt = $conn->prepare($sql_insert);
-$stmt->bindValue(1, $name);
-$stmt->bindValue(2, $email);
-$stmt->bindValue(3, $date);
-$stmt->bindValue(4, $country);
-$stmt->execute();
-echo "<h3>Вы зарегистрировались!</h3>";
-}
-}
-catch(Exception $e) {
-die(var_dump($e));
-}
-}
-
-$sql_select = "SELECT * FROM registration_on";
-$stmt = $conn->query($sql_select);
-$stmt->execute();
-if(isset($_POST['filter'])) {
-$gender = $_POST['country'];
-$sql_select = "SELECT * FROM registration_on WHERE country like :country";
-$stmt = $conn->prepare($sql_select);
-$stmt->execute(array(':country'=>$country.'%'));
-}
-$registrants = $stmt->fetchAll();
-if(count($registrants) > 0) {
-echo "<h2>Люди, которые зарегистрированы:</h2>";
-echo "<table>";
-echo "<tr><th>Name</th>";
-echo "<th>Email</th>";
-echo "<th>Country</th>";
-echo "<th>Date</th></tr>";
-foreach($registrants as $registrant) {
-echo "<td>".$registrant['name']."</td>";
-echo "<td>".$registrant['email']."</td>";
-echo "<td>".$registrant['country']."</td>";
-echo "<td>".$registrant['date']."</td></tr>";
-}
-echo "</table>";
-}
-else {
-echo "<h3>В настоящее время никто не зарегистрирован.</h3>";
-}
-
+<html> 
+<head> 
+<Title>Registration Form</Title> 
+<style type="text/css"> 
+body { background-color: 
+#fff; border-top: solid 10px #000; 
+color: #333; font-size: .85em; 
+margin: 20; padding: 20; 
+font-family: "Segoe UI", 
+Verdana, Helvetica, Sans-Serif; 
+} 
+h1, h2, h3,{ color: #000; 
+margin-bottom: 0; padding-bottom: 0; } 
+h1 { font-size: 2em; } 
+h2 { font-size: 1.75em; } 
+h3 { font-size: 1.2em; } 
+table { margin-top: 0.75em; } 
+th { font-size: 1.2em; 
+text-align: left; border: none; padding-left: 0; } 
+td { padding: 0.25em 2em 0.25em 0em; 
+border: 0 none; } 
+</style> 
+</head> 
+<body> 
+<h1>Регистрация</h1> 
+<p>Введите свое имя, адрес электронной почты, пароль, контрольный вопрос, ответ на него и нажмите кнопку <strong> Зарегестрироваться </strong>.</p> 
+<form method="post" action="index.php" 
+enctype="multipart/form-data" > 
+Имя <input type="text" 
+name="name" id="name"/></br> 
+Email <input type="text" 
+name="email" id="email"/></br> 
+Пароль <input type="text" 
+name="password" id="password"/></br> 
+Вопрос <input type="text" 
+name="vopros" id="vopros"/></br> 
+Ответ <input type="text" 
+name="otvet" id="otvet"/></br> 
+<input type="submit" 
+name="submit" value="Зарегестироваться" /> 
+<?php 
+try { 
+$conn = new PDO("sqlsrv:server = tcp:juuksqlserver.database.windows.net,1433; Database = juuksqlbase", "juuksqlserver", "200487pP"); 
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+} 
+catch (PDOException $e) { 
+print("Error connecting to SQL Server."); 
+die(print_r($e)); 
+} 
+if(!empty($_POST)) { 
+try { 
+$name = $_POST['name']; 
+$email = $_POST['email']; 
+$date = date("Y-m-d"); 
+$password = $_POST['password']; 
+$vopros = $_POST['vopros']; 
+$otvet = $_POST['otvet']; 
+$sql_insert = 
+"INSERT INTO registration_tbl (name, email, date) 
+VALUES (?,?,?)"; 
+$stmt = $conn->prepare($sql_insert); 
+$stmt->bindValue(1, $name); 
+$stmt->bindValue(2, $email); 
+$stmt->bindValue(3, $date); 
+$stmt->execute(); 
+} 
+catch(Exception $e) { 
+die(var_dump($e)); 
+} 
+echo "<h3>Your're registered!</h3>"; 
+} 
+$sql_select = "SELECT * FROM registration_tbl"; 
+$stmt = $conn->query($sql_select); 
+$registrants = $stmt->fetchAll(); 
+if(count($registrants) > 0) { 
+echo "<h2>People who are registered:</h2>"; 
+echo "<table>"; 
+echo "<tr><th>Name</th>"; 
+echo "<th>Email</th>"; 
+echo "<th>Password</th>"; 
+echo "<th>vopros</th>"; 
+echo "<th>otvet</th>"; 
+echo "<th>Date</th></tr>"; 
+foreach($registrants as $registrant) { 
+echo "<tr><td>".$registrant['name']."</td>"; 
+echo "<td>".$registrant['email']."</td>"; 
+echo "<td>".$registrant['date']."</td></tr>"; 
+} 
+echo "</table>"; 
+} else { 
+echo "<h3>No one is currently registered.</h3>"; 
+} 
 ?>
