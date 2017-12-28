cbf252b3a5b442b07826d545aefc05611f1eca68
juukin <32163888+juukin@users.noreply.github.com>
2017-12-28 14:26:14 +0300

    Update createtable.php

--- createtable.php
+++ createtable.php
@@ -5,27 +5,19 @@ try {
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 $sql = "CREATE TABLE registration_too(
-
 id INT NOT NULL IDENTITY(1,1),
-
 PRIMARY KEY(id),
 name VARCHAR(30),
 email VARCHAR(30),
 password VARCHAR(30),
 tovar VARCHAR(10),
-
 date DATE)»;
-
 $conn->query($sql);
-
 echo «<h3>Таблица создана.</h3>»;
 }
-catch (PDOException $e) {
-
+catch (PDOException $e) 
 print("Ошибка подключения к SQL Server.");
-
 die(print_r($e));
-
 }
 
 ?>
