7c8e1f85d36af8d56c45a18f20509c119342b767
juukin <32163888+juukin@users.noreply.github.com>
2018-01-10 01:01:48 +0400

    Update createtable.php

--- createtable.php
+++ createtable.php
@@ -7,10 +7,11 @@ try {
 $sql = "CREATE TABLE registration_too(
 id INT NOT NULL IDENTITY(1,1),
 PRIMARY KEY(id),
+tireqty VARCHAR(30),
+oilqty VARCHAR(30),
+sparkqty VARCHAR(30),
+address VARCHAR(30),
 name VARCHAR(30),
-email VARCHAR(30),
-password VARCHAR(30),
-tovar VARCHAR(10),
 date DATE)»;
 $conn->query($sql);
 echo «<h3>Таблица создана.</h3>»;
