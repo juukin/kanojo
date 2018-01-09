85984bbc5f7fb2d22583af79aa08124e93e3d256
juukin <32163888+juukin@users.noreply.github.com>
2018-01-10 00:27:29 +0400

    Update index.php

--- index.php
+++ index.php
@@ -37,4 +37,85 @@
  
 </body>
 </html>
-
+<?php
+//Создаем короткие имена переменных, используя длинную форму записи.
+ 
+  $tireqty = $HTTP_POST_VARS['tireqty'];
+  $oilqty = $HTTP_POST_VARS['oilqty'];
+  $sparkqty = $HTTP_POST_VARS['sparkqty'];
+  $address = $HTTP_POST_VARS['address'];
+?>
+<html>
+<head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+<title>Универмаг Краснодар</title>
+</head>
+<body>
+<h1>Универмаг Краснодар</h1>
+<h2>Ваш заказ</h2>
+ 
+<?php
+ 
+//вычисляем общее количество товара 
+  $totalqty = 0;
+  $totalqty = $tireqty + $oilqty + $sparkqty;
+ 
+// Проверяем на пустой заказ
+if ($totalqty == 0)
+{
+    echo '<font color=red>';
+    echo 'Вы ничего не заказали!!!';
+    echo '</font>';
+    exit;
+}
+  echo '<p>Результаты Вашего заказа:</p>';
+  echo $tireqty . ' - сыра;</br>';
+  echo $oilqty . ' - растительного масла;</br>';
+  echo $sparkqty . ' - майонеза;</br>';
+  echo 'Ваш адрес:  ' . "$address</br>";
+  echo 'Ваш заказ: '. "$totalqty</br>";
+ 
+  $totalamount = 0.00;
+  // Расчет итоговой суммы с учетом цен в прайс-листе
+ 
+  define('TIREPRICE',100); 
+  define('OILPRICE',10);
+  define('SPARKPRICE',4);
+ 
+  $totalamount =  $tireqty * TIREPRICE 
+    + $oilqty * OILPRICE
+    + $sparkqty * SPARKPRICE;
+  echo 'Итого: '.number_format($totalamount,3).' руб'.'</br>'; 
+ 
+  $taxrate = 0.10;  // Местный налог с продаж составляет 10%
+  $totalamount = $totalamount * (1 + $taxrate);
+  echo 'Всего, включая налог с продаж: '. 
+  number_format($totalamount,3).' руб'.'<br>';
+  echo "<p>Заказ обработан: "; echo date('H:i, jS F');
+  $date = date('H:i, jS F');
+   
+// открываем файл, указав абсолютный путь /var/www/html/orders/orders.txt 
+  @ $fp = fopen("/var/www/html/orders/orders.txt", "a");
+ 
+flock($fp, 2); // блокируем файл – файл недоступен другим
+//Проверяем, открылся ли файл. Если мы вошли внутрь файла, 
+то $fp имеет значение //TRUE
+if (!$fp)
+ {
+  echo '<p><strong>Вы не можете сейчас сделать заказ – 
+  база данных недоступна!.</strong></p>';
+  exit;
+ }
+//Если файл доступен – идет запись в строку для каждого заказа
+$outputstring=$date."\t".$tireqty."tire\t"
+.$oilqty."oil\t"
+.$sparkqty."spark\t\$".$totalamount."\t".$address."\n";
+// записываем данные в файл 
+fwrite($fp, $outputstring);
+ 
+flock($fp, 3); // Снимаем блокировку файла
+fclose($fp); // закрываем файл
+#echo phpinfo();
+?>
+</body>
+</html>
