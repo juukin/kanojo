7386b28ba9575c2fb545102cc589bf52badbec38
juukin <32163888+juukin@users.noreply.github.com>
2018-01-10 02:34:37 +0400

    Update index.php

--- index.php
+++ index.php
@@ -20,20 +20,46 @@ text-align: left; border: none; padding-left: 0; }
 td { padding: 0.25em 2em 0.25em 0em; 
 border: 0 none; } 
 </style> 
-</head> 
-<body> 
-<h1>Register here!</h1> 
-<p>Fill in your name and 
-email address, then click <strong>Submit</strong> 
-to register.</p> 
-<form method="post" action="index.php" 
-enctype="multipart/form-data" > 
-Name <input type="text" 
-name="name" id="name"/></br> 
-Email <input type="text" 
-name="email" id="email"/></br> 
-<input type="submit" 
-name="submit" value="Submit" /> 
+</head>
+<body>
+<h1>Универмаг Краснодар</h1>
+<h2>Результаты заказа</h2>
+ 
+<form action="php_var.php" method=post>
+<table border=1>
+<tr bgcolor=#cccccc>
+  <td width=150>Товар</td>
+  <td width=150>Количество</td>
+</tr>
+<tr>
+  <td>Сыр</td>
+  <td align="center"><input type="text" name="tireqty" size= "3" maxlength="3"></td>
+</tr>
+<tr>
+  <td>Растительное масло</td>
+  <td align= "center"><input type="text" name="oilqty" size="3" maxlength="3"></td>
+</tr>
+<tr>
+  <td>Майонез</td>
+  <td align="center"><input type="text" name="sparkqty" size= "3" maxlength="3"></td>
+</tr>
+<tr>
+  <td>Ваш адрес</td>
+  <td align="center"><input type="text" name="address"></td>
+</tr>
+  <tr>
+  <td>Ваше имя</td>
+  <td align="center"><input type="text" name="name"></td>
+    <tr>
+  <td>Ваш телефон</td>
+  <td align="center"><input type="text" name="phone"></td>
+</tr>
+<tr>
+  <td colspan="2" align="center"><input type="submit" value= "Подтвердить заказ"></td>
+</tr>
+</table>
+</form>
+ <?php  
 </form> 
 <?php 
 try { 
@@ -46,16 +72,23 @@ die(print_r($e));
 } 
 if(!empty($_POST)) { 
 try { 
-$name = $_POST['name']; 
-$email = $_POST['email']; 
+$tireqty = $_POST['tireqty']; 
+$oilqty = $_POST['oilqty'];
+$sparkqty = $_POST['sparkqty'];
+$adress = $_POST['adress'];
+$name = $_POST['name'];
+$phone = $_POST['phone'];
 $date = date("Y-m-d"); 
 $sql_insert = 
 "INSERT INTO registration_tbl (name, email, date) 
 VALUES (?,?,?)"; 
 $stmt = $conn->prepare($sql_insert); 
-$stmt->bindValue(1, $name); 
-$stmt->bindValue(2, $email); 
-$stmt->bindValue(3, $date); 
+$stmt->bindValue(1, $tireqty); 
+$stmt->bindValue(2, $oilqty); 
+$stmt->bindValue(3, $sparkqty);
+$stmt->bindValue(3, $adress);
+$stmt->bindValue(3, $name);
+$stmt->bindValue(3, $phone);
 $stmt->execute(); 
 } 
 catch(Exception $e) { 
@@ -69,12 +102,20 @@ $registrants = $stmt->fetchAll();
 if(count($registrants) > 0) { 
 echo "<h2>People who are registered:</h2>"; 
 echo "<table>"; 
-echo "<tr><th>Name</th>"; 
-echo "<th>Email</th>"; 
+echo "<tr><th>tireqty</th>"; 
+echo "<th>oilqty</th>"; 
+echo "<th>sparkqty</th>";
+echo "<th>adress</th>";
+echo "<th>name</th>";
+echo "<th>phone</th>";
 echo "<th>Date</th></tr>"; 
 foreach($registrants as $registrant) { 
-echo "<tr><td>".$registrant['name']."</td>"; 
-echo "<td>".$registrant['email']."</td>"; 
+echo "<tr><td>".$registrant['tireqty']."</td>"; 
+echo "<td>".$registrant['oilqty']."</td>";
+echo "<td>".$registrant['sparkqty']."</td>";
+echo "<td>".$registrant['adress']."</td>";
+echo "<td>".$registrant['name']."</td>";
+echo "<td>".$registrant['[phone']."</td>";
 echo "<td>".$registrant['date']."</td></tr>"; 
 } 
 echo "</table>"; 
