650fbba607ef305d43af329ae618aff5a633d29d
juukin <32163888+juukin@users.noreply.github.com>
2018-01-10 02:52:55 +0400

    Update index.php

--- index.php
+++ index.php
@@ -1,19 +1,33 @@
-<html>
-<head>
-<meta http-equiv="content-type" content="text/html; charset=UTF-8" > 
-  <title>Универмаг Краснодар</title>
-</head>
-<body>
-<h1>Универмаг Краснодар</h1>
-<h2>Результаты заказа</h2>
- 
+<html> 
+<head> 
+<Title>Registration Form</Title> 
+<style type="text/css"> 
+body { background-color: 
+#fff; border-top: solid 10px #000; 
+color: #333; font-size: .85em; 
+margin: 20; padding: 20; 
+font-family: "Segoe UI", 
+Verdana, Helvetica, Sans-Serif; 
+} 
+h1, h2, h3,{ color: #000; 
+margin-bottom: 0; padding-bottom: 0; } 
+h1 { font-size: 2em; } 
+h2 { font-size: 1.75em; } 
+h3 { font-size: 1.2em; } 
+table { margin-top: 0.75em; } 
+th { font-size: 1.2em; 
+text-align: left; border: none; padding-left: 0; } 
+td { padding: 0.25em 2em 0.25em 0em; 
+border: 0 none; } 
+</style> 
+</head> 
+<body> 
+<h1>Register here!</h1> 
+<p>Fill in your name and 
+email address, then click <strong>Submit</strong> 
+to register.</p> 
 <form method="post" action="index.php" 
 enctype="multipart/form-data" > 
-<table border=1>
-<tr bgcolor=#cccccc>
-  <td width=150>Товар</td>
-  <td width=150>Количество</td>
-</tr>
   <td>Сыр</td>
   <td align="center"><input type="text" name="tireqty" size= "3" maxlength="3"></td>
 </tr>
