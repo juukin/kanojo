a0dedc582909766fbb529c33e251e7aa30d6eb1c
juukin <32163888+juukin@users.noreply.github.com>
2018-01-10 13:55:28 +0300

    Update createtable.php

--- createtable.php
+++ createtable.php
@@ -3,13 +3,16 @@
 try {
     $conn = new PDO("sqlsrv:server = tcp:juuksqlserver.database.windows.net,1433; Database = juuksqlbase", "juuksqlserver", "200487pP");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
     
     
     
     
     
-$sql = "CREATE TABLE registration_top(
+    
+    
+    
+    
+    $sql = "CREATE TABLE registration_top(
 id INT NOT NULL IDENTITY(1,1),
 PRIMARY KEY(id),
 tire VARCHAR(30),
@@ -25,6 +28,11 @@ catch (PDOException $e)
 print("Ошибка подключения к SQL Server.");
 die(print_r($e));
 }
+    
+    
+    
+    
+
 
 ?>
 
