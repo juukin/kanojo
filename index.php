783a830f76ff65626ccbb6cb5d40c98c4c0612fc
juukin <32163888+juukin@users.noreply.github.com>
2018-01-10 14:33:29 +0300

    Update index.php

--- index.php
+++ index.php
@@ -61,7 +61,7 @@ $adress = $_POST['adress'];
 $name = $_POST['name'];
 $phone = $_POST['phone']; 
 $sql_insert = 
-"INSERT INTO registration_tbl (tireqty, oilqty, sparkqty, adress, name, phone) 
+"INSERT INTO registration_tov (tireqty, oilqty, sparkqty, adress, name, phone) 
 VALUES (?,?,?,?,?,?)"; 
 $stmt = $conn->prepare($sql_insert); 
 $stmt->bindValue(1, $tireqty); 
