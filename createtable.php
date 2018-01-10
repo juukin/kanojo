ddbdab21d8cf163798f6efca24a0827144ed11dc
juukin <32163888+juukin@users.noreply.github.com>
2018-01-10 15:05:22 +0300

    Update createtable.php

--- createtable.php
+++ createtable.php
@@ -6,7 +6,7 @@ try {
     
  
 $sql = "CREATE TABLE registration_top(
-id INT NOT NULL IDENTITY(1,1)",
+id INT NOT NULL IDENTITY(1,1),
 PRIMARY KEY(id);
 tireqty VARCHAR(30);
 oilqty VARCHAR(30);
@@ -15,7 +15,7 @@ address VARCHAR(30);
 name VARCHAR(30);
 phone VARCHAR(30);
 $conn->query($sql);
-echo «<h3>Таблица создана.</h3>»;
+echo «<h3>Таблица создана.</h3>»;"
 }
 catch (PDOException $e) 
 print("Ошибка подключения к SQL Server.");
