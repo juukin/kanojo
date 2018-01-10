25e794021982209c7f5135beb2e8edeac52d1999
juukin <32163888+juukin@users.noreply.github.com>
2018-01-10 13:54:16 +0300

    Update createtable.php

--- createtable.php
+++ createtable.php
@@ -4,6 +4,11 @@ try {
     $conn = new PDO("sqlsrv:server = tcp:juuksqlserver.database.windows.net,1433; Database = juuksqlbase", "juuksqlserver", "200487pP");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+    
+    
+    
+    
+    
 $sql = "CREATE TABLE registration_top(
 id INT NOT NULL IDENTITY(1,1),
 PRIMARY KEY(id),
