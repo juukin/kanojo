352329acdb49a68d3bf3ca28f5a34210f072f1d1
juukin <32163888+juukin@users.noreply.github.com>
2018-01-10 15:32:29 +0300

    Update createtable.php

--- createtable.php
+++ createtable.php
@@ -7,12 +7,12 @@ $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $sql = "CREATE TABLE registration_top( 
 id INT NOT NULL IDENTITY(1,1) 
 PRIMARY KEY(id), 
-tireqty VARCHAR(30); 
-oilqty VARCHAR(30); 
-sparkqty VARCHAR(30);
-adress VARCHAR(30);
-name VARCHAR(30);
-phone VARCHAR(30);" 
+tireqty VARCHAR(30), 
+oilqty VARCHAR(30), 
+sparkqty VARCHAR(30),
+adress VARCHAR(30),
+name VARCHAR(30),
+phone VARCHAR(30)"; 
 $conn->query($sql); 
 } 
 catch (PDOException $e) { 
