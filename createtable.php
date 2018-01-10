38893bfa26d51d7d149f5e8e2461982a9f1ec95a
juukin <32163888+juukin@users.noreply.github.com>
2018-01-10 15:06:58 +0300

    Update createtable.php

--- createtable.php
+++ createtable.php
@@ -14,12 +14,12 @@ sparkqty VARCHAR(30);
 address VARCHAR(30);
 name VARCHAR(30);
 phone VARCHAR(30);
-$conn->query($sql)";
+$conn->query($sql);
 echo «<h3>Таблица создана.</h3>»;
 }
 catch (PDOException $e) 
 print("Ошибка подключения к SQL Server.");
-die(print_r($e));
+die(print_r($e))";
 }
 
 ?>
