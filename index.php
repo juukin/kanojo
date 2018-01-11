af8cb5cd63af9108dd6f92c5d27415950f3b8c34
juukin <32163888+juukin@users.noreply.github.com>
2018-01-12 02:45:26 +0400

    Update index.php

--- index.php
+++ index.php
@@ -28,12 +28,18 @@
 	to register.</p>
 	<form method="post" action="index.php" 
 enctype="multipart/form-data" >
-Сыр<input type="text" 
-name="tireqty" size= "3" maxlength="3" id="tireqty"/></br>
-Масло<input type="text" 
-name="oilqty" size= "3" maxlength="3" id="oilqty"/></br>
-Майонез<input type="text" 
-name="sparkqty" size= "3" maxlength="3" id="sparkqty"/></br>
+Сыр<input type="text"
+	  name="tireqty" tireqty = "50" id="tireqty"/></br>
+	  $a = 50;
+		tireqty = $a;
+Масло<input type="text"
+	    name="oilqty" oilqty = "70" id="oilqty"/></br>
+	$b = 50;
+	oilqty = $b
+Майонез<input type="text"
+	      name="sparkqty" sparkqty = "100" id="sparkqty"/></br>
+$c = 100;
+sparkqty = $c
 adress<input type="text" 
 name="adress" id="adress"/></br>
 name<input type="text" 
