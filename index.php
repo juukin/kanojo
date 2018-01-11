b7e8f8e6a99a6fe241a7df6fc7323e07cf46c824
juukin <32163888+juukin@users.noreply.github.com>
2018-01-12 01:40:47 +0400

    Update index.php

--- index.php
+++ index.php
@@ -29,11 +29,11 @@
 	<form method="post" action="index.php" 
 	enctype="multipart/form-data" >
 	Сыр<input type="text" 
-	name="tireqty" tireqty = 100 size= "3" maxlength="3" id="tireqty"/></br>
+	name="tireqty"  value "tireqty" = 100 size= "3" maxlength="3" id="tireqty"/></br>
 	Масло<input type="text" 
-	name="oilqty" $oilqty = 50 size= "3" maxlength="3" id="oilqty"/></br>
+	name="oilqty"  value "oilqty" = 50 size= "3" maxlength="3" id="oilqty"/></br>
 	Майонез<input type="text" 
-	name="sparkqty" $sparkqty = 40 size= "3" maxlength="3" id="sparkqty"/></br>
+	name="sparkqty" value "sparkqty" = 40 size= "3" maxlength="3" id="sparkqty"/></br>
 	adress<input type="text" 
 	name="adress" id="adress"/></br>
 	name<input type="text" 
