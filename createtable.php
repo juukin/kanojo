2508672adf5b4bebca909d72a6ac34c5f83c1375
juukin <32163888+juukin@users.noreply.github.com>
2018-01-12 01:54:48 +0400

    Update createtable.php

--- createtable.php
+++ createtable.php
@@ -7,9 +7,9 @@ $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $sql = "CREATE TABLE top123( 
 id INT NOT NULL IDENTITY(1,1) 
 PRIMARY KEY(id), 
-tireqty VARCHAR(30), 
-oilqty VARCHAR(30), 
-sparkqty VARCHAR(30),
+tireqty = 50, 
+oilqty = 40, 
+sparkqty = 30,
 adress VARCHAR(30),
 name VARCHAR(30),
 phone VARCHAR(30)
