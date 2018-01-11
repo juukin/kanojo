11c312b574250d9f5ec783907dd71f2295c85181
juukin <32163888+juukin@users.noreply.github.com>
2018-01-12 02:31:39 +0400

    Update index.php

--- index.php
+++ index.php
@@ -94,6 +94,7 @@
 	echo "<td>".$registrant['oilqty']."</td>";
 	echo "<td>".$registrant['sparkqty']."</td>";
 	echo 'Ваш заказ: '. "$totalqty</br>";
+	echo array_sum(array_column($_SESSION['products'], 'p_qty'));
 	echo "<td>".$registrant['adress']."</td>";
 	echo "<td>".$registrant['name']."</td>";
 	echo "<td>".$registrant['phone']."</td></tr>";
