a27742915191eeb933445c802f25aefa2f64418f
juukin <32163888+juukin@users.noreply.github.com>
2018-01-13 23:01:40 +0400

    Update createtable.php

--- createtable.php
+++ createtable.php
@@ -1,24 +1,26 @@
 <?php 
-echo "123456"; 
-try { 
-$conn = new PDO("sqlsrv:server = tcp:juuksqlserver.database.windows.net,1433; Database = juuksqlbase", "juuksqlserver", "200487pP"); 
-$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+	echo "123456"; 
+	try { 
+	$conn = new PDO("sqlsrv:server = tcp:juuksqlserver.database.windows.net,1433; Database = juuksqlbase", "juuksqlserver", "200487pP"); 
+	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+	
+	$sql = "CREATE TABLE top123( 
+	id INT NOT NULL IDENTITY(1,1) 
+	PRIMARY KEY(id), 
+	tireqty VARCHAR(30), 
+	oilqty VARCHAR(30), 
+	sparkqty VARCHAR(30),
+	adress VARCHAR(30),
+	name VARCHAR(30),
+	phone VARCHAR(30)
+	summa VARCHAR(100))"; 
+	$conn->query($sql); 
+	} 
+	catch (PDOException $e) { 
+	print("Error connecting to SQL Server."); 
+	die(print_r($e)); 
+	} 
+	echo"123"; 
+	?>
 
-$sql = "CREATE TABLE top123( 
-id INT NOT NULL IDENTITY(1,1) 
-PRIMARY KEY(id), 
-tireqty VARCHAR(30), 
-oilqty VARCHAR(30), 
-sparkqt VARCHAR(30),
-adress VARCHAR(30),
-name VARCHAR(30),
-phone VARCHAR(30))"; 
-$conn->query($sql); 
-} 
-catch (PDOException $e) { 
-print("Error connecting to SQL Server."); 
-die(print_r($e)); 
-} 
-echo"123"; 
-?>
 
