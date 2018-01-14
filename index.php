76e38674a21833e6f77aa7989ed1199fa7b05275
juukin <32163888+juukin@users.noreply.github.com>
2018-01-14 23:30:41 +0400

    Update index.php

--- index.php
+++ index.php
@@ -28,11 +28,11 @@
 	to register.</p>
 	<form method="post" action="index.php" 
 	enctype="multipart/form-data" >
-	Сыр<input type="text" 
+	Сыр 50 рублей<input type="text" 
 	name="tireqty" size= "3" maxlength="3" id="tireqty"/></br>
-	Масло<input type="text" 
+	Масло 70 рублей<input type="text" 
 	name="oilqty" size= "3" maxlength="3" id="oilqty"/></br>
-	Майонез<input type="text" 
+	Майонез 100 рублей<input type="text" 
 	name="sparkqty" size= "3" maxlength="3" id="sparkqty"/></br>
 	adress<input type="text" 
 	name="adress" id="adress"/></br>
