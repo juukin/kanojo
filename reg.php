26610690a6686bd6c6c292ba02a4c0b1ac9357b3
juukin <32163888+juukin@users.noreply.github.com>
2018-04-09 23:31:40 +0400

    Create reg.php

---
+++ reg.php
@@ -0,0 +1,34 @@
+<html> 
+<head> 
+<meta charset="utf-8"> 
+Регистрация 
+</head> 
+<body> 
+
+<centre><h3> 
+Регистрация нового аккаунта.</h3></centre> 
+<form action = "" method = "get"> 
+<label form = "Name">Имя</label> 
+<input type = "text" name = "Name" id = "Name"><br> 
+<label form = "Lname">Фамилия</label> 
+<input type = "text" name = "Lname" id = "Lname"><br> 
+<label form = "Login">Логин</label> 
+<input type = "text" name = "Login" id = "Login"><br> 
+<label form = "Password">Пароль</label><br> 
+<input type = "text" name = "Password" id = "Password"><br> 
+Выберите секретный вопрос и дайте на него ответ. 
+<select> 
+<option>Нет вопроса</option> 
+<option>Имя вашего первого животного</option> 
+<option>Ваш родной город</option> 
+<option>Девичья фамилия матери</option> 
+</select><br> 
+<label form = "Otvet">Ответ:</label> 
+<input type = "text" name = "Otvet" id = "Otvet"><br> 
+</form> 
+
+<centre><input type = "button" value = "Зарегестрироваться"></centre><br> 
+
+</body> 
+
+</html>
