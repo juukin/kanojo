2fb086bea35814b8db2a7d374f85439a7cfdde47
juukin <32163888+juukin@users.noreply.github.com>
2018-04-10 20:21:22 +0400

    Update index.php

--- index.php
+++ index.php
@@ -66,53 +66,7 @@
     {
        
         echo 'Кнопка нажата!';
-	$sql_insert = 
-	"INSERT INTO top123 (tireqty, oilqty, sparkqty, adress, name, phone) 
-	VALUES (?,?,?,?,?,?)"; 
-	$stmt = $conn->prepare($sql_insert); 
-	$stmt->bindValue(1, $tireqty); 
-	$stmt->bindValue(2, $oilqty); 
-	$stmt->bindValue(3, $sparkqty);
-	$stmt->bindValue(4, $adress);
-	$stmt->bindValue(5, $name);
-	$stmt->bindValue(6, $phone);
-	$stmt->execute();
-	 
-		
-	catch(Exception $e) { 
-	die(var_dump($e)); 
-	} 
-	echo "<h3>Your're registered!</h3>"; 
-	}
-	$sql_select = "SELECT * FROM top123"; 
-$stmt = $conn->query($sql_select); 
-$registrants = $stmt->fetchAll(); 
-if(count($registrants) > 0) { 
-echo "<h2>People who are registered:</h2>"; 
-echo "<table>"; 
-echo "<tr><th>tireqty</th>"; 
-echo "<th>oilqty</th>"; 
-echo "<th>sparkqty</th>";
-echo "<th>adress</th>";
-echo "<th>name</th>";
-echo "<th>phone</th></tr>";
-foreach($registrants as $registrant) { 
-echo "<tr><td>".$registrant['tireqty']."</td>"; 
-echo "<td>".$registrant['oilqty']."</td>";
-echo "<td>".$registrant['sparkqty']."</td>";
-echo 'Ваш заказ: '. "$totalqty</br>";
-echo 'Общая сумма заказа: '. "$x</br>";
-echo "<td>".$registrant['adress']."</td>";
-echo "<td>".$registrant['name']."</td>";
-echo "<td>".$registrant['phone']."</td></tr>";
-	echo rand() . "\n";
-	echo rand() . "\n";
-	echo rand(5, 15);
-} 
-echo "</table>"; 
-} else { 
-echo "<h3>No one is currently registered.</h3>"; 
-}
+	
 	?>
 	</body>
 	</html>
