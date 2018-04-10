6179b72e982f1367a9d69774a2900fcfd4b607e9
juukin <32163888+juukin@users.noreply.github.com>
2018-04-10 20:38:24 +0400

    Update index.php

--- index.php
+++ index.php
@@ -10,13 +10,13 @@
        
     <form method="POST">
     Ваш баланс: <name = "balance"> <br>
-        <label form = "stock1">Акция 1</label>
+        <label form = "stock1">Акции Apple 288€ за шт.</label>
         <input type = "text" name = "stock1" id = "stock1"><br>
-        <label form = "stock2">Акция 2</label>
+        <label form = "stock2">Акции Sony 155€ за шт.</label>
         <input type = "text" name = "stock2" id = "stock2"><br>
-        <label form = "stock3">Акция 3</label>
+        <label form = "stock3">Акции Microsoft 201€ за шт.</label>
         <input type = "text" name = "stock3" id = "stock3"><br>
-        <label form = "stock4">Акция 4</label>
+        <label form = "stock4">Акции Tesla 115€ за шт.</label>
         <input type = "text" name = "stock4" id = "stock4"><br>
         <input type = "submit" value = "Купить" name = "buy">
 	</form>
