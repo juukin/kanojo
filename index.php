506c02cc53d1cbdf2d4de7cedff2655a0b35a413
juukin <32163888+juukin@users.noreply.github.com>
2018-04-10 23:15:46 +0400

    Update index.php

--- index.php
+++ index.php
@@ -34,7 +34,7 @@
 	
 	$balance = 40000;
 	$totalst = 0; 
-	$totalst = $s1 + $s2 + $s3 + $s4;
+	$totalst = ($s1 + $s2 + $s3 + $s4) - $balance;
 	$st = (($s1 * 288) + ($s2 * 155) + ($s3 * 201) + ($s4 * 115)) - ($balance);
 	    if( isset( $_POST['balance'] ) )
     {
@@ -44,7 +44,7 @@
 	    if( isset( $_POST['buy'] ) )
     {
        
-        echo "Сделка прошла успешно! Ваш баланс: $st";
+        echo "Сделка прошла успешно! Ваш баланс: $totalst";
  }
 	
 		     
