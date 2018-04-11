e2153a8d1a3702fb782f35cdf53f7cf65cdfdb1c
juukin <32163888+juukin@users.noreply.github.com>
2018-04-11 17:21:19 +0400

    Create vhod.php

---
+++ vhod.php
@@ -0,0 +1,23 @@
+<html> 
+<head> 
+<meta charset="utf-8"> 
+Вход 
+</head> 
+<body> 
+
+<centre><h3> 
+
+Пожалуйста, введите ваш логин и пароль.</h3></centre> 
+<form action = "" method = "get"> 
+<label form = "Login">Логин</label> 
+<input type = "text" name = "Login" id = "Login"><br> 
+<label form = "Password">Пароль</label> 
+<input type = "password" name = "Password" id = "Password"> 
+</form> 
+<form>
+<input type="button" value="Войти" onClick='location.href="index.php"'>
+</form>
+
+</body> 
+
+</html>
