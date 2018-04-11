a39dee15c5b82c554913e567ce7f9cc4c5682b65
juukin <32163888+juukin@users.noreply.github.com>
2018-04-11 17:24:58 +0400

    Create glav.php

---
+++ glav.php
@@ -0,0 +1,16 @@
+<html> 
+<head> 
+<meta charset="utf-8"> 
+Добро пожаловать в Finance Trade! Пожалуйста, выполните вход или зарегестрируйтесь.
+</head> 
+<body> 
+<form>
+<input type="button" value="Войти" onClick='location.href="vhod.php"'>
+</form>
+<form>
+<input type="button" value="Зарегестрироваться" onClick='location.href="reg.php"'>
+</form>
+
+</body>
+
+</html>
